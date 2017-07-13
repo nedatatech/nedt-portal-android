a33eb91a0ed0e3452e8a1d41bde9236df75f23e1
shawn <shawnhenrikson1@gmail.com>
2017-07-13 01:18:32 -0400

    2nd commit. Added new activities for each button in the main activity. Wrote basic functions in MainActivity.Java for starting them. Tested working on my Galaxy S8+. 6 warnings that should disappear as the project progresses.

--- app/src/main/java/com/nedatatech/datatechportal/MainActivity.java
+++ app/src/main/java/com/nedatatech/datatechportal/MainActivity.java
@@ -1,13 +1,37 @@
 package com.nedatatech.datatechportal;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  }
+
+  public void customerButton(View view){
+    Intent customerStart = new Intent(this, CustomerActivity.class);
+    startActivity(customerStart);
+  }
+
+  public void jobButton(View view){
+    Intent jobStart = new Intent(this, JobActivity.class);
+    startActivity(jobStart);
+  }
+
+  public void inventoryButton(View view){
+    Intent inventoryStart = new Intent(this, InventoryActivity.class);
+    startActivity(inventoryStart);
+  }
+
+  public void companyButton(View view){
+    Intent companyStart = new Intent(this, CompanyActivity.class);
+    startActivity(companyStart);
   }
 }
