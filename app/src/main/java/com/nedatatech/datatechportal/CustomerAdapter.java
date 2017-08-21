7e642bf4b9419c066a90aff8f1c149486db64029
shawn <shawnhenrikson1@gmail.com>
2017-08-20 20:29:42 -0400

    Search in the actual search activity is working based on ID only. Need to do more to have it search based on more criteria and tweak the layout and simplify how cluttered the search view is.

--- app/src/main/java/com/nedatatech/datatechportal/CustomerAdapter.java
+++ app/src/main/java/com/nedatatech/datatechportal/CustomerAdapter.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 
 
 public class CustomerAdapter extends ArrayAdapter<Customer> {
+
   public CustomerAdapter(Context context, ArrayList<Customer> customers) {
     super(context, 0, customers);
   }
--- app/src/main/java/com/nedatatech/datatechportal/CustomerSearchActivity.java
+++ app/src/main/java/com/nedatatech/datatechportal/CustomerSearchActivity.java
@@ -5,6 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class CustomerSearchActivity extends AppCompatActivity {
@@ -20,11 +25,13 @@ public class CustomerSearchActivity extends AppCompatActivity {
   private EditText searchCityText;
   private EditText searchStateText;
   private EditText searchZipText;
+  private ListView custResultView;
 
   private long searchID;
   private DatabaseOperations dataOps;
-  private DatabaseHelper dbHelper; // Not sure if needed here.
-  private Customer custSearchResult; // Is this the right type for the results? Array List maybe?
+  private Customer custSearchResult; // Some of these may need to be public if access ends up being needed from, other classes.
+  private ArrayList<Customer> custResultList;
+  private CustomerAdapter custAdapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +39,8 @@ public class CustomerSearchActivity extends AppCompatActivity {
     setContentView(R.layout.activity_customer_search);
     dataOps = new DatabaseOperations(this);
     dataOps.openDB(); // Needs to be on a different thread for performance. ASyncTask??
+    custResultView = (ListView) findViewById(R.id.custSearchResults_listView);
+    custResultList = new ArrayList<>(); // Will need to get the method that searches, to iterate through the results and for each result add the customer to the list.
 
     searchIDText = (EditText) findViewById(R.id.custSearchID_editText);
     searchFNameText = (EditText) findViewById(R.id.custSearchFName_editText);
@@ -47,7 +56,8 @@ public class CustomerSearchActivity extends AppCompatActivity {
     searchButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) { // Will need to call the methods from the to do on this classes file.
-
+        // Need to clear the list view first in order to free it for a new search without having to leave and reenter the activity.
+        searchCustomers();
       }
     });
 
@@ -61,21 +71,28 @@ public class CustomerSearchActivity extends AppCompatActivity {
     });
 
 // ToDo Write Methods for searching based on whether a user has entered certain search criteria on certain lines in the layout. Switch or IF Else??
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   }
+
+  public void searchCustomers() { // May be better or easier to do this with a combination of if and switch statements.
+    searchID = Long.parseLong(searchIDText.getText().toString()); // Error if the line is actually empty??
+    if(searchID > 0) { // Need a way of checking if the ID is null or has a value.
+      custSearchResult = dataOps.getCustomer(searchID); // Gets the customer result based on ID.
+      custResultList.add(custSearchResult); // Adds the customer object to the customer array. This will need to be dynamically repeated for other search criteria.
+      custAdapter = new CustomerAdapter(this, custResultList);
+      custResultView.setAdapter(custAdapter);
+    } else {
+      Toast.makeText(this, "Need to Fill in the ID Field!", Toast.LENGTH_LONG).show();
+      // ToDo This toast will never show because there is a fatal error when it tries to search with an empty field. Need to Fix.
+    }
+  }
+
+
+
+
+
+
+
+
+
+
 }
