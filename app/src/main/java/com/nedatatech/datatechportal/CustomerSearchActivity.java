9de2c4ae723e711bec34834c054d91ce23197019
shawn <shawnhenrikson1@gmail.com>
2017-08-23 18:50:10 -0400

    Drop down set up and working/tested. Next step is  updating search methods to properly use it.

--- app/src/main/java/com/nedatatech/datatechportal/CustomerSearchActivity.java
+++ app/src/main/java/com/nedatatech/datatechportal/CustomerSearchActivity.java
@@ -1,28 +1,34 @@
 package com.nedatatech.datatechportal;
 
 import android.os.Bundle;
+import android.provider.BaseColumns;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 
-public class CustomerSearchActivity extends AppCompatActivity {
-  // ToDo Finish setting up code for the new drop down parameter list thats been added to the layout.
+public class CustomerSearchActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+  // ToDo Finish tying up drop down and search method to new search in operation class once that search is updated also.
 
   private Button searchButton;
   private Button cancelButton;
-  private EditText searchIDText;
+  private EditText searchIDText; // Remove here and in layout file when all new search functions are updated.
   private EditText searchParamText;
+  private ListView custResultView;
   private Spinner paramSpinner;
 
-  private ListView custResultView;
-
+  private String[] spinnerItems = {"ID", "First Name", "Last Name", "Email", "Phone", "Street", "City", "State", "Zip Code"};
+  private String searchType;
+  private ArrayAdapter<String> spinnerAdapter;
   private long searchID;
   private DatabaseOperations dataOps;
   private Customer custSearchResult; // Some of these may need to be public if access ends up being needed from, other classes.
@@ -41,6 +47,12 @@ public class CustomerSearchActivity extends AppCompatActivity {
     searchIDText = (EditText) findViewById(R.id.custSearchID_editText);
     searchParamText = (EditText) findViewById(R.id.custSearchParam_editText);
 
+    paramSpinner = (Spinner) findViewById(R.id.custSearch_spinner);
+    spinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, spinnerItems);
+    spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    paramSpinner.setAdapter(spinnerAdapter);
+    paramSpinner.setOnItemSelectedListener(this);
+
     searchButton = (Button) findViewById(R.id.custSearch_button);
     searchButton.setOnClickListener(new View.OnClickListener() {
       @Override
@@ -57,12 +69,63 @@ public class CustomerSearchActivity extends AppCompatActivity {
         finish();
       }
     });
+  }
+
+  @Override
+  public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+    switch(position) { // Tested by printing string selections to edit text in layout. works perfectly. Rewrite search in database operations to work this way now.
+      case 0:
+        searchType = BaseColumns._ID;
+
+        break;
+      case 1:
+        searchType = DatabaseContract.CustomerColumns.COLUMN_FIRST_NAME;
+
+        break;
+      case 2:
+        searchType = DatabaseContract.CustomerColumns.COLUMN_LAST_NAME;
+
+        break;
+      case 3:
+        searchType = DatabaseContract.CustomerColumns.COLUMN_EMAIL;
+
+        break;
+      case 4:
+        searchType = DatabaseContract.CustomerColumns.COLUMN_PHONE;
+
+        break;
+      case 5:
+        searchType = DatabaseContract.CustomerColumns.COLUMN_STREET;
+
+        break;
+      case 6:
+        searchType = DatabaseContract.CustomerColumns.COLUMN_CITY;
+
+        break;
+      case 7:
+        searchType = DatabaseContract.CustomerColumns.COLUMN_STATE;
+
+        break;
+      case 8:
+        searchType = DatabaseContract.CustomerColumns.COLUMN_ZIPCODE;
+
+        break;
+      // Is a default needed?
+    }
 
   }
 
+
+  @Override
+  public void onNothingSelected(AdapterView<?> parent) {
+    // Code would go here if the drop down could have nothing selected or if needed to do something when it disappears.
+    // This method has to exist for the android framework.
+  }
+
+  // Need to rewrite this method also to work with the new spinner drop down.
   public void searchCustomers() { // May be better or easier to do this with a combination of if and switch statements.
-    if (searchIDText.getText().toString().trim().length() != 0) { // Need to do better error handling. crashes when searching for a deleted index also index higher than exists.
-      searchID = Long.parseLong(searchIDText.getText().toString()); // Error if the line is actually empty??
+    if (searchParamText.getText().toString().trim().length() != 0) { // Need to do better error handling. crashes when searching for a deleted index also index higher than exists.
+      searchID = Long.parseLong(searchParamText.getText().toString()); // Error if the line is actually empty??
       if (searchID > 0) { // Could do the if conditions based on the current size of the array index.
         custResultList.clear(); // Clears the array which clears the list view so the new result can be displayed.
         custSearchResult = dataOps.getCustomer(searchID); // Gets the customer result based on ID.
@@ -71,11 +134,9 @@ public class CustomerSearchActivity extends AppCompatActivity {
         custResultView.setAdapter(custAdapter);
       }
     } else {
-      Toast.makeText(this, "Need to Fill in the ID Field!", Toast.LENGTH_LONG).show();
+      Toast.makeText(this, "Need to Fill in the Search Field!", Toast.LENGTH_LONG).show();
       // ToDo Set up String labels for the fields in the adapter for the list view to display. i.e. ID, Name, Email, etc...
       // ToDo Still errors when the entered ID is greater than the actual size of the database or the array here??
     }
   }
-
-
 }
